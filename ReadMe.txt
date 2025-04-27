588780a3c90117c163d45e82bbb41f2c03d99d37
emily-allman <emily.allman@btinternet.com>
2025-04-27 21:54:34 +0100

    Files added

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+An example of all the "components" used to run a single simulation
+
+How to submit jobs on bluebear: https://docs.bear.bham.ac.uk/bluebear/jobs/
+Run order -> simulation_launch.sh, paper_packing_launch.sh (wait for previous to finish, can take a while), continuous_graph_launch.sh
+
+Simulation output can be viewed using paraview https://www.paraview.org/ 
---
+++ generator_sliding.py
@@ -0,0 +1,289 @@
+"""
+@author: Emily Allman
+"""
+
+#------------------------------------------------------------------------------------------
+# Calling in Libraries
+#------------------------------------------------------------------------------------------
+
+import gmsh                    # mesh generation
+import numpy as np             # for numerical operations
+import os                      # for file operations
+import sympy                   # for symbolic mathematics
+from jinja2 import Template    # for templating
+import glob                    # for file pattern matching
+from natsort import natsorted  # for natural sorting
+import toml                    # for configuration
+
+if gmsh.isInitialized():
+    gmsh.finalize()
+gmsh.initialize()
+
+
+#------------------------------------------------------------------------------------------
+# Container Geometry Specifications - defining for container and mesh
+#------------------------------------------------------------------------------------------
+
+# MCC particle size ~0.5 mm for 10% fill rate roughly 680,000 particles
+
+# cylinder dimensions
+z0_cylinder         = 0                 #m
+x0_cylinder         = 0                 #m
+y0_cylinder         = 0                 #m
+lc_cylinder         = 1e-7              #-
+cylinder_height     = 0.08               #m  
+
+# inface dimensions
+z0_inface           = 0.065      #m  # haven't changed this
+x0_inface           = 0                 #m
+y0_inface           = 0                 #m
+lc_inface           = 1e-7              #-
+
+# Mesh Configuration
+cylinder_mesh_max   = 0.005             #-
+cylinder_mesh_min   = 0.0               #-
+
+inface_mesh_max     = 0.05             #-
+inface_mesh_min     = 0.0               #-
+
+#------------------------------------------------------------------------------------------
+# Simulation Specifications - timestop, particle proerties...
+#------------------------------------------------------------------------------------------
+
+timestep            = 0.5e-5             #s
+dumptime            = 0.1             #s
+ontime              = 12.5              #s
+filltime            = 2                 #s
+settletime          = 2                 #s
+w                   = 392.322            #rad/s from 200 rad/s to 60hz
+
+density             = 1580             #kg/m^3
+youngs_modulus      = 5e6               #Pa
+poisson_ratio       = 0.4               #-
+
+sliding_pw          = 0.720222               #-
+sliding_pp_min      = 0.1               #-
+sliding_pp_max      = 0.8               #-
+
+restitution_pp_min  = 0.00              #-
+restitution_pp_max  = 1.00              #-
+num_studies         = 8                #-
+
+number_of_seeds     = 5
+
+restitution_pp      = 0.4
+restitution_pw      = 0.4
+
+# fricPP = 0.720222           
+# fricPW = 0.720222           
+# fricPSW = 0.720222           
+# fric = 0.720222           
+
+fricRollPP = 0.043350           
+fricRollPW = 0.043350           
+# fricRollPSW = 0.043350           
+# fricRoll = 0.043350 
+
+corPP = 0.664838           
+corPW = 0.664838           
+# corPSW = 0.664838          
+# cor = 0.664838           
+
+cohPP = 0             
+cohPW = 0             
+# cohPSW = 0             
+# coh = 0  
+
+#------------------------------------------------------------------------------------------
+# Slurm Job Launch Specifications
+#------------------------------------------------------------------------------------------
+
+job_runtime         = "100:00:00"        # hr:min:sec
+ntasks              = int(8)
+
+#------------------------------------------------------------------------------------------
+# Preliminary Calculations
+#------------------------------------------------------------------------------------------
+
+input_ontime        = np.round(np.ceil(ontime/timestep)*timestep,8)
+input_filltime      = np.round(np.ceil(filltime/timestep)*timestep,8)
+input_settletime    = np.round(np.ceil(settletime/timestep)*timestep,8)
+oscillation_period  = 2*np.pi/w
+
+cylinder_radii      = np.round(0.03, 8)
+inface_radii        = np.round(cylinder_radii - 0.0005,8)
+
+sliding_pp          = np.linspace(sliding_pp_min, sliding_pp_max, num_studies)
+
+seeds = [0]*number_of_seeds
+
+for k in range(number_of_seeds):
+    seeds[k] = sympy.prime(2000 + k)
+
+#------------------------------------------------------------------------------------------
+# Open Files
+#------------------------------------------------------------------------------------------
+
+with open(os.path.join("mesh1", "cylinder.geo"), 'r') as f:
+    cylinder = f.read()
+
+with open(os.path.join("mesh1", "inface.geo"), 'r') as f:
+    inface = f.read()
+
+with open(os.path.join("mesh1","shake.sim"), 'r') as f:
+    simulation = f.read()
+
+with open(os.path.join("mesh1","batch_launch.sh"), 'r') as f:
+    batch_launch = f.read()
+
+with open(os.path.join("mesh1", "particles.sim"), 'r') as f:
+    particles = f.read()
+
+#------------------------------------------------------------------------------------------
+# File Generation
+#------------------------------------------------------------------------------------------
+# List of N values to iterate over
+N_values = [35700, 53550, 71400, 89250, 107100, 124950, 142800, 160650, 178500]
+# N_values = [17850, 35700, 53550, 71400, 89250, 107100, 124950, 142800, 160650, 178500]
+
+#------------------------------------------------------------------------------------------
+# Updated File Generation with N values
+#-------------------------
+for N in N_values:
+    input_ontime = np.round(np.ceil(ontime/timestep)*timestep,8)
+    input_filltime = np.round(np.ceil(filltime/timestep)*timestep,8)
+    input_settletime = np.round(np.ceil(settletime/timestep)*timestep,8)
+    oscillation_period = 2*np.pi/w
+    cylinder_radii = np.round(0.03, 8)
+    inface_radii = np.round(cylinder_radii - 0.0005, 8)
+    sliding_pp = np.linspace(sliding_pp_min, sliding_pp_max, num_studies)
+    seeds = [0] * number_of_seeds
+    for k in range(number_of_seeds):
+        seeds[k] = sympy.prime(2000 + k)
+    
+    for seed in seeds:
+        j2_cylinder = [0] * num_studies
+        j2_inface = [0] * num_studies
+        j2_simulation = [0] * num_studies
+        j2_batch_launch = [0] * num_studies
+        j2_particles = [0] * num_studies 
+        newpath = [0] * num_studies # added
+        for i in range(num_studies):
+            # setting up jinja dictionaries for text replacement of template files
+            cylinder_mesh_data = { "cylinder_height": cylinder_height,
+                                   "cylinder_radius": cylinder_radii,
+                                   "z_0": z0_cylinder, 
+                                   "y_0": y0_cylinder,
+                                   "x_0": x0_cylinder,
+                                   "lc": lc_cylinder,
+                                   "mesh_max": cylinder_mesh_max,
+                                   "mesh_min": cylinder_mesh_min,
+                                   "Algor": 6
+                                   }
+            
+            inface_mesh_data = { "inface_radius": inface_radii, 
+                                "z_0": z0_inface, 
+                                "y_0": y0_inface, 
+                                "x_0": x0_inface, 
+                                "lc": lc_inface, 
+                                "mesh_max": inface_mesh_max, 
+                                "mesh_min": inface_mesh_min, 
+                                "Algor": 6 }
+            simulation_data = { "timestep": timestep, 
+                               "dumptime": dumptime, 
+                               "filltime": input_filltime, 
+                               "ontime": input_ontime, 
+                               "settletime": input_settletime, 
+                               "number_particles": N,  
+                               "youngs_modulus": youngs_modulus, 
+                               "poisson_ratio": poisson_ratio,
+                               "sliding_pp": sliding_pp[i], 
+                               "sliding_pw": sliding_pw, 
+                               "fricRollPP": fricRollPP, 
+                               "fricRollPW": fricRollPW, 
+                               "restitution_pp": restitution_pp, 
+                               "restitution_pw": restitution_pw, 
+                               "cohPP": cohPP, 
+                               "cohPW": cohPW, 
+                               "seed": seed, 
+                               "period": oscillation_period }
+            
+            particles_data = {"restitution_pp": restitution_pp, 
+                              "restitution_pw": restitution_pw,
+                              "fricRollPP": fricRollPP,
+                              "fricRollPW": fricRollPW,
+                              "corPP": corPP,
+                              "corPW": corPW,
+                              "cohPP": cohPP,
+                              "cohPW": cohPW,
+                              }
+
+            batch_launch_data = { "runtime": job_runtime, 
+                                 "ntasks": ntasks }
+            post_processing_data = { "cylinder_radii": cylinder_radii, 
+                                    "sliding_pp": sliding_pp[i] }
+            j2_cylinder[i] = Template(cylinder)
+            j2_inface[i] = Template(inface)
+            j2_simulation[i] = Template(simulation)
+            j2_batch_launch[i] = Template(batch_launch)
+            j2_particles[i] = Template(particles) 
+            
+            # Creating file paths
+            study_directory = os.path.join(f"{N:.0f}_particles", f"seed_{seed}")
+            if not os.path.exists(study_directory):
+                os.makedirs(study_directory)
+            simulation_newpath = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}")
+            if not os.path.exists(simulation_newpath):
+                os.makedirs(simulation_newpath)
+            batch_launch_newpath = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}")
+            if not os.path.exists(batch_launch_newpath):
+                os.makedirs(batch_launch_newpath)
+            mesh_newpath = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}", "mesh")
+            if not os.path.exists(mesh_newpath):
+                os.makedirs(mesh_newpath)
+            inface_newpath = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}", "mesh")
+            if not os.path.exists(inface_newpath):
+                os.makedirs(inface_newpath)
+            toml_path = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}")
+            if not os.path.exists(toml_path):
+                os.makedirs(toml_path)
+            particles_newpath = os.path.join(study_directory, f"sliding_{sliding_pp[i]:.2f}", "mesh")
+            if not os.path.exists(particles_newpath):
+                os.makedirs(particles_newpath)
+            
+            # Creating new simulation files
+            cylinder_geo_file_new = os.path.join(mesh_newpath, "shake_cylinder.geo")
+            inface_geo_file_new = os.path.join(inface_newpath, "inface.geo")
+            simulation_file_new = os.path.join(simulation_newpath, "shake.sim")
+            batch_launch_file_new = os.path.join(batch_launch_newpath, "batch_launch.sh")
+            toml_file = os.path.join(toml_path, "data.toml")
+            particles_sim_file_new = os.path.join(particles_newpath, "particles.sim")
+            
+            # Writing parameters to new files
+            with open(cylinder_geo_file_new, 'w') as f:
+                f.write(j2_cylinder[i].render(cylinder_mesh_data))
+            with open(inface_geo_file_new, 'w') as f:
+                f.write(j2_inface[i].render(inface_mesh_data))
+            with open(simulation_file_new, 'w') as f:
+                f.write(j2_simulation[i].render(simulation_data))
+            with open(batch_launch_file_new, 'w') as f:
+                f.write(j2_batch_launch[i].render(batch_launch_data))
+            with open(toml_file, 'w') as f:
+                toml.dump(post_processing_data, f)
+            with open(particles_sim_file_new, 'w') as f:
+                f.write(j2_particles[i].render(particles_data))  # added
+            
+            # Creating STL files from the gmsh geo files
+            gmsh.open(cylinder_geo_file_new)
+            gmsh.open(inface_geo_file_new)
+        
+        #--------------------------------
+        # Launch Slurm Jobs
+        #- 
+        glob_input = os.path.join(study_directory, "sliding_*")
+        directories = natsorted([k for k in glob.glob(glob_input)])
+        for directory in directories:
+            launch_file = os.path.join(directory, "batch_launch.sh")
+            cmd = f"sbatch --output={directory}/slurm-%j.out {launch_file} {directory}"
+            print(cmd)
+            os.system(cmd)
